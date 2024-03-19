e9a48e570a80a5ee5ebe19bd7ece6892e5c3a340
leenayeonnn <skdus4190@gmail.com>
2024-03-19 14:53:56 +0900

    Feat: 배열 최대값

---
+++ src/main/java/com/example/chap02/FindMaxScore.java
@@ -0,0 +1,38 @@
+package com.example.chap02;
+
+import java.util.Scanner;
+
+public class FindMaxScore {
+    public static void main(String[] args) {
+        int[] scores = input();
+        System.out.println("max score : " + max(scores));
+    }
+
+    public static int[] input() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("input student num : ");
+        int studentNum = sc.nextInt();
+
+        int[] scores = new int[studentNum];
+
+        for (int i = 1; i <= studentNum; i++) {
+            System.out.print("input student" + i + " score : ");
+            scores[i - 1] = sc.nextInt();
+        }
+
+        sc.close();
+        return scores;
+    }
+
+    private static int max(int[] scores) {
+        int max = 0;
+
+        for (int score : scores) {
+            if (score > max) {
+                max = score;
+            }
+        }
+        return max;
+    }
+}
