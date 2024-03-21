71673ee01f18c33ac2c4d07a96a09c4a5f8f08b5
leenayeonnn <skdus4190@gmail.com>
2024-03-21 11:36:37 +0900

    Feat: Queue 간단 버전

---
+++ src/main/java/com/example/chap04/MyQueue.java
@@ -0,0 +1,122 @@
+package com.example.chap04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MyQueue {
+    int tail;
+    int head;
+    int size;
+    int[] queueArr;
+
+    public MyQueue(int size) {
+        this.head = -1;
+        this.tail = -1;
+        this.size = size;
+        queueArr = new int[size];
+    }
+
+    public void enqueue(int num) {
+
+        if (useAll()) {
+            System.out.println("모든 큐 공간을 사용했습니다.");
+        } else if (isFull()) {
+            System.out.println("큐가 가득 찼습니다.");
+        } else {
+            if (isFirstEnqueue()) {
+                head++;
+            }
+            queueArr[++tail] = num;
+        }
+
+    }
+
+    private boolean useAll() {
+        return head == size;
+    }
+
+    private boolean isFull() {
+        return tail == size - 1;
+    }
+
+    private boolean isFirstEnqueue() {
+        return head == -1 && tail == -1;
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("큐가 비어있습니다.");
+            return -1;
+        } else {
+            int num = queueArr[head];
+            queueArr[head] = 0;
+            head++;
+            return num;
+        }
+    }
+
+    public int peek() {
+
+        if (isEmpty()) {
+            System.out.println("큐가 비어있습니다.");
+            return -1;
+        } else {
+            return queueArr[head];
+        }
+    }
+
+    private boolean isEmpty() {
+        return head > tail || (head == -1 && tail == -1);
+    }
+
+    public void showQueue() {
+        System.out.println(Arrays.toString(queueArr));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Queue의 사이즈 : ");
+        int size = sc.nextInt();
+
+        MyQueue myQueue = new MyQueue(size);
+
+        int choice;
+        int num;
+
+        roof:
+        while (true) {
+            System.out.println("1.Enqueue, 2.Dequeue, 3.peek, 4.isEmpty, 5.exit");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("넣을 숫자 : ");
+                    num = sc.nextInt();
+                    myQueue.enqueue(num);
+                    break;
+                case 2:
+                    num = myQueue.dequeue();
+                    System.out.println("dequeue " + num);
+                    break;
+                case 3:
+                    num = myQueue.peek();
+                    System.out.println("head : " + num);
+                    break;
+                case 4:
+                    if (myQueue.isEmpty()) {
+                        System.out.println("스택이 비었습니다.");
+                    } else {
+                        System.out.println("스택에 데이터가 있습니다.");
+                    }
+                    break;
+                case 5:
+                    break roof;
+                default:
+                    System.out.println("잘못된 입력");
+            }
+            myQueue.showQueue();
+        }
+
+        sc.close();
+    }
+}
